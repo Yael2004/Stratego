2425e51f306dcd365a2acac0d3264ac77fde30a0
Gabriel Armas <armasgabriel29@gmail.com>
2024-10-06 11:37:43 -0600

    add: MainWindow viewmodel and the base viewmodel class were implemented and are already associated with the LogIn user control

---
+++ StrategoClient/StrategoApp/ViewModel/LogInViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace StrategoApp.ViewModel
+{
+    public class LogInViewModel : ViewModelBase
+    {
+        private string _username;
+        private string _password;
+        private bool _isViewEnabled;
+        private string _errorMessage;
+        private MainWindowViewModel _mainWindowViewModel;
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsViewEnabled
+        {
+            get { return _isViewEnabled; }
+            set
+            {
+                _isViewEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand LogInCommand { get; }
+        public ICommand SignUpCommand { get; }
+
+        public LogInViewModel(MainWindowViewModel mainWindowViewModel)
+        {
+            _mainWindowViewModel = mainWindowViewModel;
+            LogInCommand = new ViewModelCommand(ExecuteLogInCommand, CanExecuteLogInCommand);
+            SignUpCommand = new ViewModelCommand(p => ExecuteSignUpCommand());
+        }
+
+        public LogInViewModel()
+        {
+            LogInCommand = new ViewModelCommand(ExecuteLogInCommand, CanExecuteLogInCommand);
+            SignUpCommand = new ViewModelCommand(p => ExecuteSignUpCommand());
+        }
+
+        private void ExecuteSignUpCommand()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanExecuteLogInCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExecuteLogInCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StrategoClient/StrategoApp/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using StrategoApp.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategoApp.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private object _currentViewModel;
+
+        public object CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            CurrentViewModel = new LogInViewModel(this);
+        }
+    }
+}
