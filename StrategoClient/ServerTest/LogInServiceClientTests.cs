a51f120b4f82e7bbf89c9c00d8f952263a916615
Gabriel Armas <armasgabriel29@gmail.com>
2024-10-25 08:39:15 -0600

    Server tests has been implemented in client

---
+++ StrategoClient/ServerTest/LogInServiceClientTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StrategoApp.LogInService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTest
+{
+    public class TestLogInServiceCallback : ILogInServiceCallback
+    {
+        public OperationResult LastOperationResult { get; private set; }
+        public PlayerDTO LastPlayerInfo { get; private set; }
+
+        public void LogInResult(OperationResult result)
+        {
+            LastOperationResult = result;
+        }
+
+        public void AccountInfo(PlayerDTO player)
+        {
+            LastPlayerInfo = player;
+        }
+    }
+
+    [TestClass]
+    public class LogInServiceClientTests
+    {
+        private TestLogInServiceCallback _callback;
+        private LogInServiceClient _client;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _callback = new TestLogInServiceCallback();
+
+            var instanceContext = new InstanceContext(_callback);
+            _client = new LogInServiceClient(instanceContext);
+        }
+
+        [TestMethod]
+        public async Task TestLogIn_Success()
+        {
+            await _client.LogInAsync("pikachu999@gmail.com", "407d8a1148a12157d5c1509f328461f6488a405bd4d361363b22be741fe1b1ee");
+
+            Assert.AreEqual("Login successful", _callback.LastOperationResult.Message);
+        }
+
+        [TestMethod]
+        public async Task TestLogIn_InvalidCredentials()
+        {
+            await _client.LogInAsync("logInTest@gmail.com", "hashed_password");
+
+            Assert.AreEqual("Invalid credentials", _callback.LastOperationResult.Message);
+        }
+
+        [TestMethod]
+        public async Task TestLogIn_InBlankCredentials()
+        {
+            await _client.LogInAsync("", "");
+
+            Assert.AreEqual("Invalid credentials", _callback.LastOperationResult.Message);
+        }
+
+        [TestMethod]
+        public async Task TestLogIn_VerifyAccountInfo()
+        {
+            await _client.LogInAsync("pikachu999@gmail.com", "407d8a1148a12157d5c1509f328461f6488a405bd4d361363b22be741fe1b1ee");
+
+            Assert.AreEqual("Pikachu999", _callback.LastPlayerInfo.Name);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            if (_client.State == CommunicationState.Opened)
+            {
+                _client.Close();
+            }
+        }
+    }
+
+}
---
+++ StrategoClient/ServerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ServerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("ServerTest")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("527e9bbe-db30-44f0-b8f1-8a472dd12383")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
