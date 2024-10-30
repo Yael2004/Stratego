673aab7f6303b42256c835cee12ed6469c9f15df
Yael <yaelalfredosalazaraguilar14@gmail.com>
2024-10-29 22:58:44 -0600

    add: Room section added

---
+++ StrategoClient/StrategoApp/ViewModel/RoomViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace StrategoApp.ViewModel
+{
+    public class RoomViewModel : ViewModelBase
+    {
+        private readonly MainWindowViewModel _mainWindowViewModel;
+        public ICommand ExecuteBackToLobbyCommand { get; }
+        public RoomViewModel(MainWindowViewModel mainWindowViewModel)
+        {
+            ExecuteBackToLobbyCommand = new ViewModelCommand(ExecuteBackToLobby);
+            _mainWindowViewModel = mainWindowViewModel;
+
+        }
+
+        private void ExecuteBackToLobby(Object obj)
+        {
+            try
+            {
+                _mainWindowViewModel.ChangeViewModel(LobbyViewModel.Instance(_mainWindowViewModel));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
