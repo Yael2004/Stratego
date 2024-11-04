48a8dbbc1bc5a841b01445054560ece802a538a5
Yael <yaelalfredosalazaraguilar14@gmail.com>
2024-11-04 17:29:06 -0600

    add: class test of profile service cliente added

---
+++ StrategoClient/ServerTest/ProfileServiceClientTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StrategoApp.ProfileService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTest
+{
+    public class TestProfileDataServiceCallback : IProfileDataServiceCallback
+    {
+        public PlayerStatisticsResponse PlayerStatisticsResponse { get; private set; }
+        public PlayerInfoResponse PlayerInfoResponse { get; private set; }
+
+        public void PlayerStatistics(PlayerStatisticsResponse playerStatistics)
+        {
+            PlayerStatisticsResponse = playerStatistics;
+        }
+
+        public void PlayerInfo(PlayerInfoResponse playerInfo)
+        {
+            PlayerInfoResponse = playerInfo;
+        }
+    }
+
+    [TestClass]
+    public class ProfileServiceClientTest
+    {
+        private TestProfileDataServiceCallback _callback;
+        private ProfileDataServiceClient _client;
+
+        [TestInitialize]
+        public void SetUp() 
+        { 
+            _callback = new TestProfileDataServiceCallback();
+            var instanceContext = new InstanceContext(_callback);
+            _client = new ProfileDataServiceClient(instanceContext);
+        }
+
+        [TestMethod]
+        public async Task GetPlayerStatistics_ShouldReturnStatistics()
+        {
+            await _client.GetPlayerStatisticsAsync(1017);
+
+            Assert.AreEqual(_callback.PlayerStatisticsResponse.Statistics.WonGames, 3);
+        }
+    }
+}
